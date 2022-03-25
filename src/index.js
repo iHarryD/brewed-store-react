6d8787c100b6bfe8663cad5065c7c48bed3ed506
iHarryD <anuraagvr555@gmail.com>
2022-03-26 02:31:40 +0530

    edited basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="brewed-store">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
